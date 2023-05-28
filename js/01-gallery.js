df836df6b946198e5072256b5f8559d03f1ad059
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-05-28 14:12:04 +0300

    Finished the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
-import { galleryItems } from './gallery-items.js';
+//Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+//Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+//Реалізація делегування на ul.gallery і отримання url великого зображення.
+//Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
+//Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+//Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+//Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям.
+//Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+//Розмітка елемента галереї
+//Посилання на оригінальне зображення повинно зберігатися в data-атрибуті
+//source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML
+//теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+//<li class="gallery__item">
+//  <a class="gallery__link" href="large-image.jpg">
+//    <img
+//      class="gallery__image"
+//      src="small-image.jpg"
+//      data-source="large-image.jpg"
+//      alt="Image description"
+//    />
+//  </a>
+//</li>
+//
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`
+);
+
+galleryContainer.insertAdjacentHTML("beforeend", markup.join(""));
+galleryContainer.addEventListener("click", onClick);
+
+const instance = basicLightbox.create(`<img src="">`, {
+  onShow: (instance) => {
+    window.addEventListener("keydown", onEscKeyPress);
+  },
+  onClose: (instance) => {
+    window.removeEventListener("keydown", onEscKeyPress);
+  },
+});
+
+function onClick(eve) {
+  eve.preventDefault();
+  const datasetSource = eve.target.dataset.source;
+  if (!datasetSource) {
+    return;
+  }
+  instance.element().querySelector("img").src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(eve) {
+  if (eve.code !== "Escape") {
+    return;
+  }
+  instance.close();
+}
